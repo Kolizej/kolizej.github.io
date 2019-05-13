bae9c00bc410dc1a49fc16712d06c80c97a49362
daniluk.devel@gmail.com <daniluk.devel@gmail.com>
2019-05-13 21:28:17 +0300

    -- fix button

---
+++ index.js
@@ -0,0 +1,4 @@
+function go_ios() {
+    location.href='itms-services://?action=download-manifest&url=https://kolizej.github.io/manifest.plist';
+    alert("APPLICATION IS INSTALLING. GO TO MAIN SCREEN");
+}
