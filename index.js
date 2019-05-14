bc60f262c2e33b4899c43b2e5303cffebbaf5113
Sergei Daniluk <daniluk.devel@gmail.com>
2019-05-14 16:22:51 +0300

    -- fix caption

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function go_ios() {
     location.href='itms-services://?action=download-manifest&url=https://kolizej.github.io/manifest.plist';
-    document.getElementById("btnInstall").innerText = "APPLICATION IS INSTALLING. GO TO MAIN SCREEN";
+    document.getElementById("btnInstall").innerText = "PRESS INSTALL BUTTON AND GO TO MAIN SCREEN";
     //alert("APPLICATION IS INSTALLING. GO TO MAIN SCREEN");
 }
